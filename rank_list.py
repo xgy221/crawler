bc676f5349775a59fb822d3b3436d5b5a6228d07
xgy221 <17302558812@163.com>
2018-04-09 09:35:36 +0800

    modify

--- rank_list.py
+++ rank_list.py
@@ -1,7 +1,18 @@
 import urllib.request
+import json
+import csv
 
-url = "http://fsight.qq.com/GameList?type=hotRank"
+url = "http://fsight.qq.com/GameListAjax"
 response = urllib.request.urlopen(url)
 html = response.read()
 html = html.decode('utf-8')
-print(html)
+# print(html)
+hjson = json.loads(html)
+# print(hjson)
+with open("rank_list.csv", "w", newline="") as w:
+    for i in hjson.get('ret').get('ranks'):
+        # print(str(i.get('rank'))+":"+str(i.get('game_name')))
+        writer = csv.writer(w, delimiter='\t')
+        writer.writerow(str(i.get('game_id')) + ',' + str(i.get('game_name')))
+
+
